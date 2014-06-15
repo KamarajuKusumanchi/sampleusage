37cfce0d66b7d6784fcf8a4853f686f070696030
Kamaraju Kusumanchi <kamaraju@gmail.com>
2014-06-15 19:38:28 -0400

    fixed comments

--- octave/using_intersect.m
+++ octave/using_intersect.m
@@ -2,7 +2,7 @@
 # Author : Kamaraju S. Kusumanchi
 # Email  : kamaraju at gmail dot com
 # Date (last modified) : Sun Jun 15 19:26:40 EDT 2014
-#
+
 # prevent Octave from thinking that this is a function file
 clear;
 
@@ -19,7 +19,7 @@ printf("intersection = ");
 disp(A3);
 
 # sample usage
-rajulocal@hogwarts:~/sampleusage/octave$ ./using_intersect.m
+# rajulocal@hogwarts:~/sampleusage/octave$ ./using_intersect.m
 # first set    =    1   2   3   4
 # second set   =    3   4   5   6
 # intersection =    3   4
