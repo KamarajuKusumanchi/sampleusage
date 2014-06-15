e0b7585f290bef242fe6f564153cfd850f44d726
Kamaraju Kusumanchi <kamaraju@gmail.com>
2014-06-15 19:35:04 -0400

    Added a simple octave script to show the usage of intersect function

---
+++ octave/using_intersect.m
@@ -0,0 +1,25 @@
+#! /usr/bin/octave -qf
+# Author : Kamaraju S. Kusumanchi
+# Email  : kamaraju at gmail dot com
+# Date (last modified) : Sun Jun 15 19:26:40 EDT 2014
+#
+# prevent Octave from thinking that this is a function file
+clear;
+
+A1 = [1 2 3 4];
+A2 = [3 4 5 6];
+
+A3 = intersect(A1, A2);
+
+printf("first set    = ");
+disp(A1);
+printf("second set   = ");
+disp(A2);
+printf("intersection = ");
+disp(A3);
+
+# sample usage
+rajulocal@hogwarts:~/sampleusage/octave$ ./using_intersect.m
+# first set    =    1   2   3   4
+# second set   =    3   4   5   6
+# intersection =    3   4
