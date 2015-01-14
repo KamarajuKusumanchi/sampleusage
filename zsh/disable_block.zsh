601c11826a972c22bb703b4f282e5f5e11b94307
Kamaraju Kusumanchi <kamaraju@gmail.com>
2015-01-14 00:31:26 -0500

    reformatted the comments

--- zsh/disable_block.zsh
+++ zsh/disable_block.zsh
@@ -1,6 +1,7 @@
 #! /usr/bin/zsh
 
-# If you have a block of code that you do not want to be executed but at the same time you do not want to comment it. You can use this hack.
+# If you have a block of code that you do not want to be executed but at the
+# same time you do not want to comment it. You can use this hack.
 
 if ((0==1)) then
 echo "I am in 0==1";
