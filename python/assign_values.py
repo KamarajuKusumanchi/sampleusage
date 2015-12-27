395fd62e4f1380948a25d2c545559b9b3d555b8b
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2015-12-26 23:41:52 -0500

    initial commit of assign_values.py

---
+++ python/assign_values.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+counter = 10    # An integer
+miles = 100.0   # A floating point
+name = "John"   # A string
+
+print(counter)
+print(miles)
+print(name)
+
+# demonstrate multiple assignment
+# Assign same value to several variables
+a = b = c = 1
+print(a)
+print(b)
+print(c)
+
+# assign different values to different variables
+a, b, c = 1, 2.0, "Smith"
+print(a, b, c)
+
+# sample usage:
+# % ./assign_values.py 
+# 10
+# 100.0
+# John
+# 1
+# 1
+# 1
+# 1 2.0 Smith
