8c037695b206ba8fc75cf11522c7cdd5a539e240
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2015-12-30 00:50:00 -0500

    Add new python scripts

---
+++ python2/no_header_hello.py
@@ -0,0 +1,10 @@
+print "hello world"
+
+# There is no need to have
+# #! /usr/bin/env python
+# at the beginning. There is also no need to chmod +x the file. You can simply
+# write a script and execute it as follows.
+#
+# sample usage:
+# % python no_header_hello.py 
+# hello world
