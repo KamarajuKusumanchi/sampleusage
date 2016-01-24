96fd9dabb577fefb485adbf2b9fbc110e6a1e471
KamarajuKusumanchi <kamaraju@gmail.com>
2016-01-23 23:36:21 -0500

    Initial commit of using_sample_input.py

---
+++ python2/using_sample_input.py
@@ -0,0 +1,10 @@
+hours = raw_input("Enter Hours: ")
+rate = raw_input("Enter rate: ")
+gross_pay = float(hours) * float(rate);
+print gross_pay
+
+# Sample usage:
+# % python gross_pay.py 
+# Enter Hours: 40
+# Enter rate: 10
+# 400.0
