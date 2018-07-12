9e4f92f240c6ddeec322b562753a38553566f7ea
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-07-12 12:01:59 -0400

    Added directory_exists_01.sh

---
+++ bash/directory_exists_01.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+dirs=(
+    "/bin"
+	"/blahblah"
+	)
+
+for directory in ${dirs[@]}
+do
+	if [ -d "$directory" ]; then
+		echo "$directory exists"
+	else
+		echo "$directory does not exist"
+	fi
+done
+
+# Note:- To check if a directory does not exist, use
+# if [ ! -d "$directory" ]; then
