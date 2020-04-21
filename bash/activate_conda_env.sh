e7aa9714ac7cff522552501ac6a0dc1ba1bfab52
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2020-04-21 12:22:20 -0400

    lgtm

---
+++ bash/activate_conda_env.sh
@@ -0,0 +1,10 @@
+# Snippet to activate a conda environment if necessary
+#! /usr/bin/env bash
+
+desired_env=py38
+if [ -z $CONDA_DEFAULT_ENV ] || [ $CONDA_DEFAULT_ENV != "$desired_env" ]; then
+    echo "current conda environment is $CONDA_DEFAULT_ENV. Changing it to $desired_env"
+    source activate "$desired_env"
+else
+    echo "conda environment is already set to $desired_env"
+fi
